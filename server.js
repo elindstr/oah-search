ac00dd15088ce147b82f9f1e5a996f0077232cbd
elindstr <elindstr@gmail.com>
2024-02-03 07:50:58 -0800

    reverting to port 3000

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const path = require('path')
 const parseSearchInput = require('./parser')
 
 app.use(express.static('public'))
-const port = 80
-http.listen(port, '0.0.0.0', () => {
-  // console.log(`Server running at http://0.0.0.0:${port}/`)
+// const port = 80
+// http.listen(port, '0.0.0.0', () => {
+//   // console.log(`Server running at http://0.0.0.0:${port}/`)
+// })
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
 
 // redirect /oah to the root
